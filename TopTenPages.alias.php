f38eb7516918e65e7c0e2ef8a5b08a1fa3ff18ca
paladox <thomasmulhall410@yahoo.com>
2015-09-15 19:17:45 +0100

    Add alias file and update qqq.json messages
    Adds missing alias file for special page.
    
    Update qqq.json messages.
    
    Change-Id: I7a5a548b6ca593e3b73209ba36badf85b3f4f4c6

---
+++ TopTenPages.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages for extension TopTenPages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+// @codingStandardsIgnoreFile
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'TopTenPages' => array( 'TopTenPages' ),
+);
