ca8e3ba7467ff398b22822d13a902aa7f108957c
Zoranzoki21 <zorandori4444@gmail.com>
2019-12-26 00:58:10 +0100

    Add aliases for Serbian language
    Bug: T212586
    Change-Id: Ifbc06ac8130b430045ca81e20dd7fdb8f2c93c1f

--- TopTenPages.alias.php
+++ TopTenPages.alias.php
@@ -12,3 +12,13 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'TopTenPages' => [ 'TopTenPages' ],
 ];
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'TopTenPages' => [ 'ДесетНајпосећенијихСтраница' ],
+];
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = [
+	'TopTenPages' => [ 'DesetNajposećenijihStranica' ],
+];
